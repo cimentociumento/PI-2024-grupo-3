b5edddaebe54ac0833719291f421e924ffbb9818
Davi Ulisses <davi.ulisses.ifc@gmail.com>
2025-01-12 17:03:30 -0300

    corrigidas as questões das boas praticas e também atualizei o footer forra correção de pequenos erros

--- script/script.js
+++ script/script.js
@@ -43,3 +43,28 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 console.log('Script carregado');
+
+// Botão Scroll to Top com rolagem suave
+window.onscroll = function() {
+    const scrollButton = document.getElementById("scrollToTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "flex";
+    } else {
+        scrollButton.style.display = "none";
+    }
+};
+
+document.getElementById("scrollToTop").onclick = function(e) {
+    e.preventDefault();
+    
+    const scrollDuration = 1000; // Duração da animação em ms
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+    
+    const scrollInterval = setInterval(function() {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 15);
+};
